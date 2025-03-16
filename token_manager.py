80326a3d872470fab35b62d5441b1290530956cb
titus <titus.oxx@gmail.com>
2025-03-16 09:50:52 +0100

    + Fixed getting access token

--- token_manager.py
+++ token_manager.py
@@ -1,5 +1,9 @@
-from time import time_ns
-import requests
+import hashlib
+import hmac
+import math
+import struct
+import time
+from curl_cffi import requests as curl_requests
 
 from config_manager import cM
 import logging
@@ -7,6 +11,9 @@ import logging
 
 class TokenManager:
     TOKEN_URL = 'https://open.spotify.com/get_access_token'
+
+    HOTP_SECRET = b'5507145853487499592248630329347'
+    HOTP_PERIOD = 30
 
     def __init__(self):
         self.sp_dc = None
@@ -23,13 +30,42 @@ class TokenManager:
 
         self.access_token = self.get_access_token()
 
+    @staticmethod
+    def _generate_hotp(secret: bytes, counter: int, digits: int = 6) -> str:
+        counter_bytes = struct.pack(">Q", counter)
+
+        hmac_digest = hmac.new(secret, counter_bytes, hashlib.sha1).digest()
+
+        offset = hmac_digest[-1] & 0x0F
+        binary_code = (
+                (hmac_digest[offset] & 0x7F) << 24 |
+                (hmac_digest[offset + 1] & 0xFF) << 16 |
+                (hmac_digest[offset + 2] & 0xFF) << 8 |
+                (hmac_digest[offset + 3] & 0xFF)
+        )
+
+        otp = binary_code % (10 ** digits)
+
+        return str(otp).zfill(digits)
+
     def _request_access_token(
             self,
             sp_dc: str
     ) -> tuple[str, str]:
-        token_request = requests.get(
+        timestamp = int(time.time() * 1000)
+        counter = math.floor(timestamp / 1000 / self.HOTP_PERIOD)
+        hotp = self._generate_hotp(self.HOTP_SECRET, counter)
+
+        token_request = curl_requests.get(
             url=self.TOKEN_URL,
-            headers={'Cookie': f'sp_dc={sp_dc}'}
+            params={
+                'productType': 'web-player',
+                'totp': hotp,
+                'totpVer': '5'
+            },
+            cookies={
+                'sp_dc': sp_dc,
+            }
         )
 
         if token_request.status_code != 200:
@@ -50,7 +86,7 @@ class TokenManager:
         return access_token, access_token_expire
 
     def get_access_token(self):
-        if (time_ns() // 1_000_000) >= cM.simple_get('accessTokenExpire'):
+        if int(time.time() * 1000) >= cM.simple_get('accessTokenExpire'):
             self.access_token, self.access_token_expire = self._request_access_token(self.sp_dc)
         if not self.access_token:
             return cM.simple_get('accessToken')
